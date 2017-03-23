ec0d1be6d13a3839c5008465210702d64ae3e6e9
rensu <rensu@hikvision.com>
2017-03-23 15:33:37 +0800

    [ADD]创建工程基本文件和框架

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Enterprise-Quality-
+# Enterprise-Quality
 资质检索
+
+为了我们可爱的产品经理们做的
---
+++ src/main/java/com/hikvision/rensu/EnterpriseQualityApplication.java
@@ -0,0 +1,12 @@
+package com.hikvision.rensu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseQualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseQualityApplication.class, args);
+	}
+}
