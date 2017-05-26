bb3b566509d5d927fd1a9b271e985f7dcedf8321
郎逸聪 <langyc0210@163.com>
2017-05-26 14:57:35 +0800

    [ADD] sidebar controller (javascript)

--- src/main/resources/static/scripts/global/sidebar.js
+++ src/main/resources/static/scripts/global/sidebar.js
@@ -1,7 +1,37 @@
-/*sideBarCtrl*/
 'use strict';
 
 angular.module('enterprise-quality').controller('sideBarCtrl', 
     function($scope, $location){
-    	console.log('sideBarCtrl');
+    	$scope.headerText = '检索分类';
+    	$scope.menu = [
+    		{
+    			title: '公检&国标',
+    			href: '/inspections',
+    			icon:'fa-laptop',
+    			active: false
+    		}, {
+    			title: '双证',
+    			href: '/copyright',
+    			icon:'fa-files-o',
+    			active: false
+    		}, {
+    			title: '3C',
+    			href: '/ccc',
+    			icon:'fa-th',
+    			active: false  			
+    		}
+    	];
+    	
+    	for(var i=0;i<$scope.menu.length;i++){
+    		var pattern = RegExp("\\"+$scope.menu[i].href);
+    		if(pattern.test($location.url())){
+    			$scope.menu[i].active = true;
+    		}
+    	}
+    	
+    	$scope.change = function(index){
+    		for(var i=0;i<$scope.menu.length;i++){
+    			$scope.menu[i].active = (i==index)? true: false;
+        	}
+    	}
     });
