158b93523625ad28800751f59a0c1e0e293e7937
rexren <rexrens@gmail.com>
2017-05-27 13:53:18 +0800

    [DEL] delete files associate with elasticsearch

--- src/main/java/com/hikvision/rensu/EnterpriseQualityApplication.java
+++ src/main/java/com/hikvision/rensu/EnterpriseQualityApplication.java
@@ -1,49 +1,22 @@
 package com.hikvision.rensu;
 
-import com.hikvision.rensu.cert.domain.Post;
-import com.hikvision.rensu.cert.domain.Tag;
-import com.hikvision.rensu.cert.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class EnterpriseQualityApplication implements CommandLineRunner {
+
+    static private final Logger logger = LoggerFactory.getLogger(EnterpriseQualityApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(EnterpriseQualityApplication.class, args);
     }
 
-    @Autowired
-    private PostService service;
-
-
     @Override
     public void run(String... args) throws Exception {
-        Tag tag = new Tag();
-        tag.setId("1");
-        tag.setName("tech");
-        Tag tag2 = new Tag();
-        tag2.setId("2");
-        tag2.setName("elasticsearch");
-
-        Post post = new Post();
-        post.setId("1");
-        post.setTitle("Bigining with spring boot application and elasticsearch");
-        post.setRating(9.5);
-        post.setTags(Arrays.asList(tag, tag2));
-
-        service.save(post);
-
-
-        Post post2 = new Post();
-        post2.setId("2");
-        post2.setTitle("Bigining with spring boot application");
-        post2.setTags(Arrays.asList(tag));
-        post2.setRating(7.5);
-        service.save(post2);
+        logger.debug("Here we put some pre start.");
     }
 }
