48f8642c3cdf8c9a127147df5bf611353f14f738
郎逸聪 <langyc0210@163.com>
2017-06-17 17:00:51 +0800

    [MOD] 修改获取用户信息方式

--- src/main/java/com/hikvision/rensu/cert/controller/HomeController.java
+++ src/main/java/com/hikvision/rensu/cert/controller/HomeController.java
@@ -1,26 +1,32 @@
 package com.hikvision.rensu.cert.controller;
 
 import java.security.Principal;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hikvision.rensu.cert.constant.RetStatus;
-import com.hikvision.rensu.cert.support.BaseResult;
+import com.hikvision.rensu.cert.domain.SystemUser;
+import com.hikvision.rensu.cert.service.SystemUserService;
+import com.hikvision.rensu.cert.support.AjaxResult;
 
 /**
  * Created by rensu on 17/4/1.
  */
 @Controller
 public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(InspectionController.class);
 
+	@Autowired
+	private SystemUserService systemUserService;
 	/**
 	 * 默认登陆页面
 	 * @param
@@ -46,11 +52,21 @@ public class HomeController {
 	 * @param request
 	 * @return user
 	 */
-	//TODO 只返回用户名、id和role
+	//TODO 另建class，只返回用户名、id和role
 	@ResponseBody
 	@RequestMapping("/user")
-	public Principal user(Principal user) {
-		return user;
+	public AjaxResult<SystemUser> user(Principal user) {
+		AjaxResult<SystemUser> res = new AjaxResult<SystemUser>();
+		try {
+			res.setData(systemUserService.findByName(user.getName()).get(0));
+			res.setCode(RetStatus.SUCCESS.getCode());
+			res.setMsg(RetStatus.SUCCESS.getInfo());
+		} catch (Exception e) {
+			logger.error("", e);
+			res.setCode(RetStatus.SYSTEM_ERROR.getCode());
+			res.setMsg(RetStatus.SYSTEM_ERROR.getInfo());
+		}
+		return res;
 	}
 
 	/**
