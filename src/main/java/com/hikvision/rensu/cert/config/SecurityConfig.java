2db68f3a710d987127fcf5254990b5f45320bff1
郎逸聪 <langyc0210@163.com>
2017-07-01 15:44:46 +0800

    [MOD]取消google fonts远程获取资源

--- src/main/java/com/hikvision/rensu/cert/config/SecurityConfig.java
+++ src/main/java/com/hikvision/rensu/cert/config/SecurityConfig.java
@@ -45,8 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	/* 临时方法 运行加密算法 */
 	public static void main(String[] args) {
 		PasswordEncoder encoder = new BCryptPasswordEncoder();
-		System.out.println(encoder.encode("111111"));
-		System.out.println(encoder.encode("126726015"));
+		System.out.println(encoder.encode("ga111111"));
+		System.out.println(encoder.encode("123456"));
 	}
 
 	/*These codes could be put in either @Autowired whateverName or @Override configure*/
